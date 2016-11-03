ed8c913d0e48bb7432492b176299e8df1de666f8
Roberto Gutierrez <elpayoquedibuja@gmail.com>
2016-11-03 13:33:39 +0100

    primer borrador con un setInterval funcionando

--- mi-javascript.js
+++ mi-javascript.js
@@ -1,6 +1,12 @@
-function confirmar (){
-  document.getElementById("emocion").innerHTML='Bai, \n tu carta le ha llegado correctamente al Olentzero. Y, si su amatxu o su aita no dicen otra cosa, le llegará puntualmente el 25 de diciembre.';
-  alert("hola, \n qué tal estás?");
+function conectar (){
+document.getElementById("conectar").innerHTML='Conectando con la base de datos de OLENTZERO...';
+setInterval(respuesta, 5000);
+}
+function respuesta (){
+  document.getElementById("conectar").innerHTML='...';
+  document.getElementById("ok").innerHTML='Conexión con la base de datos de OLENTZERO realizada con éxito!!';
+  document.getElementById("responder").innerHTML='Bai, su carta le ha llegado correctamente al Olentzero. Y, si su amatxu o su aita no dicen otra cosa, Olentzero pasará por su casa puntualmente el 25 de diciembre. No os preocupeis';
+
 }
 
-btn_confirmar.addEventListener("click", confirmar);
+btn_confirmar.addEventListener("click", conectar);
