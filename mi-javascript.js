578b5a0560592db4aa4ce38cfc368bdcd8fac8d3
Roberto Gutierrez <elpayoquedibuja@gmail.com>
2016-11-03 10:41:57 +0100

    Añadido el innerHTML y un olentzero en primer plano para el head

---
+++ mi-javascript.js
@@ -0,0 +1,6 @@
+function confirmar (){
+  document.getElementById("emocion").innerHTML='Bai, \n tu carta le ha llegado correctamente al Olentzero. Y, si su amatxu o su aita no dicen otra cosa, le llegará puntualmente el 25 de diciembre.';
+  alert("hola, \n qué tal estás?");
+}
+
+btn_confirmar.addEventListener("click", confirmar);
