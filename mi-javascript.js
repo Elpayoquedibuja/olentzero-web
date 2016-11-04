aa38cc210a99264a425ed9044106ee22afb28b09
Roberto Gutierrez <elpayoquedibuja@gmail.com>
2016-11-04 16:33:32 +0100

    Corregidos los paddings y algunos cambios en los textos

--- mi-javascript.js
+++ mi-javascript.js
@@ -5,7 +5,7 @@ setInterval(respuesta, 5000);
 function respuesta (){
   document.getElementById("conectar").innerHTML='...';
   document.getElementById("ok").innerHTML='Conexión con la base de datos de OLENTZERO realizada con éxito!!';
-  document.getElementById("responder").innerHTML='Bai, su carta le ha llegado correctamente al Olentzero. Y, si su amatxu o su aita no dicen otra cosa, Olentzero pasará por su casa puntualmente el 25 de diciembre. No os preocupeis';
+  document.getElementById("responder").innerHTML='"Sí, su carta le ha llegado correctamente al Olentzero. Y, si su amatxu o su aita no dicen otra cosa, Olentzero pasará por su casa puntualmente el 25 de diciembre. No os preocupeis"';
 
 }
 
